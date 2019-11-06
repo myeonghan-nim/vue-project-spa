33f3a96ac68eeca999389d987958298e314d8c37
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-06 17:24:29 +0900

    mid commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GetImage btnName="bark" @getAnimalImage="getImage"/>
+    <GetImage btnName="meow" @getAnimalImage="getImage"/>
+    <img v-bind:src="animalUrl" alt="">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import GetImage from './components/GetImage.vue'
 
 export default {
   name: 'app',
+
   components: {
-    HelloWorld
+    GetImage
+  },
+
+  methods: {
+    getImage: function(name) {
+      if (name === 'bark') {
+        const DOG_URL = 'https://dog.ceo/api/breeds/image/random'
+
+        axios.get(DOG_URL)
+          .then((response) => {
+            this.animalUrl = response.data.message
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      } else {
+        const CAT_URL = 'https://api.thecatapi.com/v1/images/search'
+
+        axios.get(CAT_URL)
+          .then((response) => {
+            this.animalUrl = response.data[0].url
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      }
+    }
+  },
+
+  data: function() {
+    return {
+      animalUrl: ''
+    }
   }
 }
 </script>
---
+++ src/components/GetCatImage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button v-on:click="getCatImage()">meow!</button>
+    <img v-bind:src="catImage">
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: function() {
+    return {
+      catImage: ''
+    }
+  },
+
+  methods: {
+    getCatImage: function() {
+      const CAT_URL = 'https://api.thecatapi.com/v1/images/search'
+
+      axios.get(CAT_URL)
+        .then((response) => {
+          this.catImage = response.data[0].url
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
